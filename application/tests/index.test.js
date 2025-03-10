d3d2d2cd973a16285b8929fb8f21bff554015338
Ubuntu <ubuntu@ip-172-31-11-136.eu-north-1.compute.internal>
2025-03-10 13:49:08 +0000

    Correcting directory structure

---
+++ application/tests/index.test.js
@@ -0,0 +1,9 @@
+const add = require("../index");
+
+test("adds 2 + 3 to equal 5", () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test("adds 0 + 0 to equal 0", () => {
+  expect(add(0, 0)).toBe(0);
+});
